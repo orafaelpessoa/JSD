9e9e62c44d66a262fc3e47c4cced2b3ec06a5750
orafaelpessoa <rafael.pessoa.alexandre@gmail.com>
2025-03-10 12:28:57 -0300

    style: logos adicionadas!

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -3,9 +3,14 @@ import IA01 from '../assets/IA01.jpeg'
 import IA02 from '../assets/IA02.jpeg'
 import IA03 from '../assets/IA03.jpeg'
 import IA04 from '../assets/IA04.jpeg'
+import JSDLOGO01 from '../assets/JSDLOGO01.jpeg'
+import JSDLOGO02 from '../assets/JSDLOGO02.jpeg'
+import JSDLOGO03 from '../assets/JSDLOGO03.jpeg'
+import JSDLOGO04 from '../assets/JSDLOGO04.jpeg'
+
 
 const ImageSlider: React.FC = () => {
-  const images = [IA01, IA02, IA03, IA04];
+  const images = [IA01, IA02, IA03, IA04, JSDLOGO01, JSDLOGO02, JSDLOGO03, JSDLOGO04];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextImage = () => {
